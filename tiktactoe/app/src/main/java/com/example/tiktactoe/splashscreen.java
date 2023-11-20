580f5436095670cac2a4fbe86609825f5cbc1371
SANJAY <VARTIKSAXENA123@GMAIL.COM>
2023-11-20 14:01:42 +0530

    Initial commit

---
+++ tiktactoe/app/src/main/java/com/example/tiktactoe/splashscreen.java
@@ -0,0 +1,31 @@
+package com.example.tiktactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashscreen.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        },5000);
+
+
+
+
+
+
+    }
+}
